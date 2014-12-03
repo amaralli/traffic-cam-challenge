3593c6446569396e5a6ea531075bccf30fdc28b2
iGuest <amaralli@uw.edu>
2014-12-02 22:59:31 -0800

    First draft, fully functional

--- js/app.js
+++ js/app.js
@@ -11,3 +11,58 @@
 //you should also write the code to filter the set of markers when the user
 //types a search phrase into the search box
 
+
+$(document).ready(function() {
+    var mapOptions = {
+        center: new google.maps.LatLng(47.6, -122.3),
+        zoom: 12
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    //google.maps.event.addDomListener(window, 'load', initialize);
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: ""
+    });
+
+    $.getJSON("http://data.seattle.gov/resource/65fc-btcc.json", function(data) {
+        //NEED TO SHOW AN ALERT IF THIS FAILS
+        data.forEach(function(value) {
+            var location = value.location;
+            var image = value.imageurl;
+            var cameraLabel = value.cameralabel;
+            var marker = new google.maps.Marker({
+                position: {
+                    lat: Number(location.latitude),
+                    lng: Number(location.longitude)
+                },
+                map: map,
+                label: cameraLabel,
+                imgURL: image.url
+            });
+
+            var clickListener = google.maps.event.addListener(marker, 'click', function () {
+                var selectedMarker = this;
+                var location = selectedMarker.getPosition();
+                map.panTo(location);
+                infoWindow.open(map, selectedMarker);
+                var content = '<p>' + selectedMarker.label + '</p>' +
+                    '<img src=' + selectedMarker.imgURL + '>'
+                infoWindow.setContent(content);
+            });
+
+            $("#search").bind('search keyup', function () {
+                var searchTerm = this.value.toLowerCase();
+                var searchLabel = marker.label.toLowerCase();
+                if(searchLabel.indexOf(searchTerm) < 0) {
+                    marker.setMap(null);
+                } else {
+                    marker.setMap(map);
+                }
+            });
+        });
+
+    });
+
+});
